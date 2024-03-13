41be5aa29f02e811672e8e5e6746b477a5c3e2dd
Daniel Fenz <daniel@fenz.io>
2024-03-14 00:43:49 +0100

    added GreateEq constant

---
+++ core/src/main/scala/gapt/expr/formula/GreaterEq.scala
@@ -0,0 +1,25 @@
+package gapt.expr.formula
+import gapt.expr.formula.constants.GreaterEqC
+import gapt.expr.Apps
+import gapt.expr.formula.fol.FOLAtom
+import gapt.expr.Expr
+import gapt.expr.formula.fol.FOLTerm
+import gapt.expr
+import gapt.expr.formula.fol.FOLExpression
+import gapt.expr.formula.fol.FOLFormula
+
+object GreaterEq {
+  def apply( a: Expr, b: Expr ): Atom = Apps( GreaterEqC( a.ty ), a, b ).asInstanceOf[Atom]
+  def apply( a: FOLTerm, b: FOLTerm ): FOLAtom =
+    apply( a, b.asInstanceOf[Expr] ).asInstanceOf[FOLAtom]
+
+  def unapply( e: Expr ): Option[( Expr, Expr )] = e match {
+    case expr.App( expr.App( GreaterEqC( _ ), a ), b ) => Some( a, b )
+    case _ => None
+  }
+  def unapply( f: FOLFormula ): Option[( FOLTerm, FOLTerm )] = unapply( f.asInstanceOf[FOLExpression] )
+  def unapply( f: FOLExpression ): Option[( FOLTerm, FOLTerm )] = f.asInstanceOf[Expr] match {
+    case Eq( a: FOLTerm, b: FOLTerm ) => Some( a, b )
+    case _                            => None
+  }
+}
