c2f26ba21a0b84c951d26c8e7b2d648d11c66035
Daniel Fenz <daniel@fenz.io>
2024-03-14 12:48:02 +0100

    added numeric types

---
+++ core/src/main/scala/gapt/logic/fol/arithmetic/types.scala
@@ -0,0 +1,7 @@
+package gapt.logic.fol.arithmetic
+
+import gapt.expr.ty.TBase
+
+object TInt extends TBase( "int", List() )
+object TRat extends TBase( "rat", List() )
+object TReal extends TBase( "real", List() )
