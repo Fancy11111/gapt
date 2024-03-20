c754190be3d2f33256a80e5f8a6f0dc5175f9139
Daniel Fenz <daniel@fenz.io>
2024-03-20 23:15:57 +0100

    added greater, lesser and lessereq

--- core/src/main/scala/gapt/logic/fol/arithmetic/Greater.scala
+++ core/src/main/scala/gapt/logic/fol/arithmetic/Greater.scala
@@ -1,5 +1,5 @@
 package gapt.logic.fol.arithmetic
-import gapt.expr.formula.constants.GreaterEqC
+import gapt.expr.formula.constants.GreaterC
 import gapt.expr.Apps
 import gapt.expr.formula.fol.FOLAtom
 import gapt.expr.Expr
@@ -9,13 +9,13 @@ import gapt.expr.formula.fol.FOLExpression
 import gapt.expr.formula.fol.FOLFormula
 import gapt.expr.formula._
 
-object GreaterEq {
-  def apply( a: Expr, b: Expr ): Atom = Apps( GreaterEqC( a.ty ), a, b ).asInstanceOf[Atom]
+object Greater {
+  def apply( a: Expr, b: Expr ): Atom = Apps( GreaterC( a.ty ), a, b ).asInstanceOf[Atom]
   def apply( a: FOLTerm, b: FOLTerm ): FOLAtom =
     apply( a, b.asInstanceOf[Expr] ).asInstanceOf[FOLAtom]
 
   def unapply( e: Expr ): Option[( Expr, Expr )] = e match {
-    case expr.App( expr.App( GreaterEqC( _ ), a ), b ) => Some( a, b )
+    case expr.App( expr.App( GreaterC( _ ), a ), b ) => Some( a, b )
     case _ => None
   }
   def unapply( f: FOLFormula ): Option[( FOLTerm, FOLTerm )] = unapply( f.asInstanceOf[FOLExpression] )
--- core/src/main/scala/gapt/logic/fol/arithmetic/Lesser.scala
+++ core/src/main/scala/gapt/logic/fol/arithmetic/Lesser.scala
@@ -1,5 +1,6 @@
+
 package gapt.logic.fol.arithmetic
-import gapt.expr.formula.constants.GreaterEqC
+import gapt.expr.formula.constants.LesserC
 import gapt.expr.Apps
 import gapt.expr.formula.fol.FOLAtom
 import gapt.expr.Expr
@@ -9,18 +10,18 @@ import gapt.expr.formula.fol.FOLExpression
 import gapt.expr.formula.fol.FOLFormula
 import gapt.expr.formula._
 
-object GreaterEq {
-  def apply( a: Expr, b: Expr ): Atom = Apps( GreaterEqC( a.ty ), a, b ).asInstanceOf[Atom]
+object Lesser {
+  def apply( a: Expr, b: Expr ): Atom = Apps( LesserC( a.ty ), a, b ).asInstanceOf[Atom]
   def apply( a: FOLTerm, b: FOLTerm ): FOLAtom =
     apply( a, b.asInstanceOf[Expr] ).asInstanceOf[FOLAtom]
 
   def unapply( e: Expr ): Option[( Expr, Expr )] = e match {
-    case expr.App( expr.App( GreaterEqC( _ ), a ), b ) => Some( a, b )
+    case expr.App( expr.App( LesserC( _ ), a ), b ) => Some( a, b )
     case _ => None
   }
   def unapply( f: FOLFormula ): Option[( FOLTerm, FOLTerm )] = unapply( f.asInstanceOf[FOLExpression] )
   def unapply( f: FOLExpression ): Option[( FOLTerm, FOLTerm )] = f.asInstanceOf[Expr] match {
-    case GreaterEq( a: FOLTerm, b: FOLTerm ) => Some( a, b )
-    case _                                   => None
+    case Lesser( a: FOLTerm, b: FOLTerm ) => Some( a, b )
+    case _                                => None
   }
 }
