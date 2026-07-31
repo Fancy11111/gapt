7b30b131be84d6074366458dc64b27fe8915d3fa
Daniel Fenz <daniel@fenz.io>
2026-07-31 14:10:33 +0200

    added arithmetic helpers

---
+++ core/src/main/scala/gapt/logic/fol/arithmetic/types.scala
@@ -0,0 +1,12 @@
+package gapt.logic.fol.arithmetic
+
+import gapt.expr.ty.TBase
+import gapt.expr.ty.Ty
+
+object Numeric {
+  def isNumeric(ty: Ty) = ty == TInt || ty == TReal || ty == TRat
+}
+
+object TInt extends TBase("int", List())
+object TRat extends TBase("rat", List())
+object TReal extends TBase("real", List())
